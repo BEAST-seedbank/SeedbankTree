0275c40200940253c315e172c038177bf651049b
jacklo225 <jacklo.2252002@gmail.com>
2024-03-15 16:55:27 -0400

    Create class to initialize SeedbankTree from Newick string

---
+++ src/seedbanktree/evolution/tree/SeedbankTreeFromNewick.java
@@ -0,0 +1,69 @@
+package seedbanktree.evolution.tree;
+
+import java.util.List;
+
+import beast.base.core.Description;
+import beast.base.core.Input;
+import beast.base.core.Input.Validate;
+import beast.base.evolution.tree.TreeParser;
+import beast.base.inference.StateNode;
+import beast.base.inference.StateNodeInitialiser;
+
+@Description("Class to initialize a SeedbankTree from single child newick tree with type metadata")
+public class SeedbankTreeFromNewick extends SeedbankTree implements StateNodeInitialiser {
+
+	public Input<String> newickStringInput = new Input<>("value",
+            "Tree in Newick format.", Validate.REQUIRED);
+
+    public Input<Boolean> adjustTipHeightsInput = new Input<>("adjustTipHeights",
+            "Adjust tip heights in tree? (Default false).", false);
+    
+    
+	public SeedbankTreeFromNewick() {
+    	m_initial.setRule(Validate.REQUIRED);
+    }
+	
+	@Override
+	public void initAndValidate() {
+		// seedbankTree inputs
+    	typeLabel = typeLabelInput.get();
+        activeTypeName = activeTypeNameInput.get();
+        dormantTypeName = dormantTypeNameInput.get();
+        
+//        processTraits(m_traitList.get());
+        
+        // parse newick string
+        TreeParser parser = new TreeParser();
+        parser.initByName(
+                "IsLabelledNewick", true,
+                "adjustTipHeights", adjustTipHeightsInput.get(),
+                "singlechild", true,
+                "newick", newickStringInput.get());
+
+        initFromFlatTree(parser, true);
+//        initOtherFromFlatTree(parser, (SeedbankTree)m_initial.get(), true);
+	}
+	
+    // Methods for StateNodeInitialiser interface
+
+	@Override
+    public void initStateNodes() {
+        assert(m_initial != null);
+        if (!(m_initial.get() instanceof SeedbankTree)) {
+        	throw new IllegalArgumentException("Attempted to use "
+                    + "seedbank tree initialiser on regular tree object");
+        }
+    	m_initial.get().assignFromWithoutID(this);
+    }
+
+    @Override
+    public void getInitialisedStateNodes(final List<StateNode> stateNodes) {
+    	assert(m_initial != null);
+        if (!(m_initial.get() instanceof SeedbankTree)) {
+        	throw new IllegalArgumentException("Attempted to use "
+                    + "seedbank tree initialiser on regular tree object");
+        }
+        stateNodes.add(m_initial.get());
+    }
+
+}
