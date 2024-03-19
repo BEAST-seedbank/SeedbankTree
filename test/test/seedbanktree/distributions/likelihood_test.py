5467b8ae898f9532568dd06d386874036b03de23
dbarrow <djb438@cornell.edu>
2024-03-19 09:27:48 -0400

    reworked rate matrix scaling

--- test/test/seedbanktree/distributions/likelihood_test.py
+++ test/test/seedbanktree/distributions/likelihood_test.py
@@ -5,13 +5,14 @@ import numpy.linalg as linalg
 class Node():
     ''' Initializes a node with given parameters.
     Arguments:
-        name: name of node (only relevant for leaves)
-        left: left child (Node)
-        right: right child (Node)
+        active: state of node, active or dormant (refers to branch above)
         branch_length: length of branch that leads to this node (float)
         branch_id: id of branch that leads to this node (int)
+    Other param
         probs: probability of observed bases beneath this node
                 [list of 4 probs for 'ACGT'] (initialized to None)
+        childs: list of children nodes
+        parent: parent node
     '''
     def __init__(self, branch_id, active, branch_length):
         self.childs = []
@@ -28,8 +29,8 @@ def likelihood(ordering, seq_list):
     TideTree model.
     NOTE:  This is not yet used in this project
     :param ordering: list of nodes in post-traverse order
-    :param char_mat: character matrix of single-cell lineage data
-    :return: log-likelihood using TideTree model
+    :param seq_list: list of sequences on leaves
+    :return: log-likelihood
     '''
     alph = 'ACGT'
     N = len(seq_list) #number of leaves
@@ -51,8 +52,13 @@ def likelihood(ordering, seq_list):
     # GTR_params_dorm = np.array([0.2,0.5,0.5,0.5,0.5,0.8]) #human random
 
 
-    P_params_active = GTR_model(pi, GTR_params_active)
-    P_params_dorm = GTR_model(pi, GTR_params_dorm)
+    #*** Code now normalizes and rescales mutation rate in GTR_model with 'scale' parameter, in line with Beast2 methods***
+    #*** Need to re-rewrite this method implementation if one wishes to again account for arbitrary rate matrices***
+    P_params_active = GTR_model(pi, GTR_params_active, 1)
+    P_params_dorm = GTR_model(pi, GTR_params_active, 0.5)
+
+
+
     LL = 0
     for i in range(n):  #for each element i in sequence
         for j in range(len(ordering)):  #for each node in tree (first N nodes are leaves
@@ -96,13 +102,15 @@ def sumLogProbs(a,b):
     else:
         return b + np.log1p(np.exp(a-b))
 
-def GTR_model(pi,params):
+def GTR_model(pi,params,scale=1):
     a,b,c,d,e,f = params
     Q = np.array([[0,a*pi[1],b*pi[2],c*pi[3]],[a*pi[0],0,d*pi[2],e*pi[3]],[b*pi[0],d*pi[1],0,f*pi[3]],[c*pi[0],e*pi[1],f*pi[2],0]])
     for i in range(4):
         Q[i,i] = -np.sum(Q[i])
+        Q[i] = -scale*Q[i]/Q[i,i]      #rescaled to expected mutation rate of 1 (per unit time)
     Evals, T = np.linalg.eig(Q)
     Ti = linalg.inv(T)  #points off
+
     P_params = Evals, T, Ti
     return P_params
 
@@ -134,35 +142,26 @@ def create_test_tree_topology(type=1):
         nodes[b].branch_length = len_b
         nodes.append(parent)
 
-    def dormant_active(a, len1=1, len2=1, len3=1):
-        '''
-        :param nodes: list of nodes
-        :param a: node to go dormant and active
-        :return: list of nodes with two new nodes along branch connected by dormancy
-        '''
-        next_num = len(nodes)
-        parent1 = Node(next_num, False, 1)
-        parent1.childs.append(nodes[a])
-        parent1.branch_length = len2
-        nodes[a].parent = parent1
-        nodes[a].branch_length = len1
-        next_num += 1
-        parent2 = Node(next_num, True, 1)
-        parent2.childs.append(parent1)
-        parent2.branch_length = len3
-        parent1.parent = parent2
-        nodes.append(parent1)
-        nodes.append(parent2)
 
+    def state_transition(a, dormant_to_active, len1 = 1):
+        next_num = len(nodes)
+        parent = Node(next_num, dormant_to_active, 1)
+        parent.childs.append(nodes[a])
+        parent.branch_length = len1
+        nodes[a].parent = parent
+        nodes.append(parent)
 
     nodes = []
     for i in range(3):
         nodes.append(Node(i, True, 1))
 
     if type == 1:   #code in different sample trees later
-        dormant_active(1)
+        #dormant_active(1)
+        state_transition(1,False,1)
+        state_transition(3,True,1)
         coalesce(0, 4)
-        dormant_active(2)
+        state_transition(2,False,1)
+        state_transition(6,True,1)
         coalesce(5, 7)
 
     if type == 2:   #code in different sample trees later
